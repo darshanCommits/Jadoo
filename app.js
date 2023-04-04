08249d32d7635a566de993b34860b7f39d27401d
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2023-04-04 22:15:40 +0530

    done with layout and styling

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 const enter = document.querySelector("#enter");
 const start = document.querySelector("#start");
 const computer = document.querySelector("#computer");
-const btn = document.querySelector("#sound-btn")
+const btn = document.querySelector("#sound-btn").childNodes;
+let isComputerOn = false;
 
 function changeScreen() {
+  document.documentElement.requestFullscreen();
   start.classList.add("slide-up");
-
   computer.classList.add("slide-down");
   computer.style.display = "grid";
+  isComputerOn = true;
 }
 
-document.addEventListener("keypress", (e) => {
-  if (computer.classList.contains("slide-down"))
-    document.getElementById(e.key).play();
+btn.forEach((x) => {
+  x.addEventListener("click", () => {
+    console.log(x.textContent);
+    document.getElementById(x.textContent.toLowerCase()).play();
+  });
 });
 
-enter.addEventListener("click", changeScreen);
+enter.addEventListener("click", changeScreen, { once: true });
+
+document.addEventListener("keypress", (e) => {
+  const audios = ["b", "c", "d", "e", "f"];
+
+  if (e.key === "Enter") {
+    enter.classList.add("active");
+    setTimeout(() => {
+      enter.classList.remove("active");
+      changeScreen();
+    }, 100);
+  }
+
+  if (isComputerOn && audios.includes(e.key))
+    document.getElementById(e.key).play();
+});
